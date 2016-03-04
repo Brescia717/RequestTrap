b4c085c6044a6d0f0b9295525cae9beb234e103a
Paul Brescia <brescia717@gmail.com>
2016-03-04 13:24:53 -0500

    Added serialization for raw_response

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,7 @@ class Request < ActiveRecord::Base
   serialize  :headers, Hash
   serialize  :cookies, Hash
   serialize  :query_params, Hash
+  serialize  :raw_response, Hash
 
   after_save :notify_watcher
 
